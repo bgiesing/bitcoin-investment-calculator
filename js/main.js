e6c07386a43fc32a76be3967ea67fa3984242374
eltonc88 <eltonchan@cal.berkeley.edu>
2015-10-11 17:43:35 -0700

    change chart to display last month exchange data

--- js/main.js
+++ js/main.js
@@ -2,20 +2,15 @@ var bitcoinCalculator = angular.module('bitcoinCalculator', ['nvd3ChartDirective
 bitcoinCalculator.controller('bitcoinController', function($scope, $http){
 
   $.ajax({
-    url: "https://bitpay.com/api/rates",
+    url: "https://bitpay.com/api/rates/usd",
     type: "GET",
     dataType: 'json',
     error: function(method, url, resp){
-      console.error( "bad request " + method + " to " + url + " resp: " + resp );
+      console.error( "bad request https://bitpay.com/api/rates/usd");
     },
     success: function(data){
       $scope.rates = data;
-      for(var i=0;i<data.length;i++){
-        if (data[i].code == "USD"){
-          $scope.currRate = data[i].rate;
-          break;
-        }
-      }
+      if (data["code"] == "USD") $scope.currRate = data["rate"];
       $scope.initalAmt  = 5000;
       $scope.newAmt     = function(price){return price/$scope.currRate * $scope.initalAmt;}
       $scope.profit     = function(price){return price/$scope.currRate * $scope.initalAmt - $scope.initalAmt;}
@@ -24,15 +19,39 @@ bitcoinCalculator.controller('bitcoinController', function($scope, $http){
     },
   });
 
-  $scope.exampleData = [{
-    "key": "Quantity",
-    "bar": true,
-    "values": [
-      [10, 20],
-      [20, 40],
-      [30, 60],
-      [40, 80],
-      [50, 100]
-    ]
-  }];
+  $scope.xAxisTickFormatFunction = function(){
+    return function(date){
+      return d3.time.format('%x')(new Date(date));
+    };
+  };
+
+  $scope.bpiEndDate = new Date();
+  $scope.bpiStartDate = new Date()
+  $scope.bpiStartDate.setYear($scope.bpiEndDate.getFullYear() - 2);
+
+
+
+  $.ajax({
+    url: "https://api.coindesk.com/v1/bpi/historical/close.json?start=" +
+      $scope.bpiStartDate.toJSON().slice(0,10) + "&" +
+      $scope.bpiEndDate.toJSON().slice(0,10),
+    type: "GET",
+    dataType: 'json',
+    error: function(resp, err){
+      console.error( "bad request http://api.coindesk.com/v1/bpi/historical/close.json");
+    },
+    success: function(data){
+      var dataArray = $.map(data.bpi, function (value, index){
+        return [[new Date(index).getTime(), value]];
+      });
+      debugger
+      $scope.bitcoinHistoricalData = [{
+        "key": "Prices",
+        "values": dataArray
+      }];
+
+      $scope.$apply()
+    },
+  });
+
 });
